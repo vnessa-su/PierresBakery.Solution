30a1150899295eb283e278da88a729d9019c1709
Vanessa Su <vnessa.su@gmail.com>
2021-05-07 13:16:36 -0400

    :recycle: Add properties and constructor for Bread

--- PierresBakery/Models/BreadMenu.cs
+++ PierresBakery/Models/BreadMenu.cs
@@ -4,12 +4,23 @@ namespace BreadMenu
 {
   public class Bread
   {
+    private decimal StandardPrice {get; set;}
+    private decimal DealPrice {get; set;}
+    private decimal NumberOfItemsForDeal {get; set;}
+
+    public Bread(decimal standardItemPrice, decimal dealPrice, decimal numberOfItemsForDeal)
+    {
+      StandardPrice = standardItemPrice;
+      DealPrice = dealPrice;
+      NumberOfItemsForDeal = numberOfItemsForDeal;
+    }
+
     public decimal GetCost(int numberOfItems)
     {
       decimal cost = 0;
-      decimal setsOfThree = Math.Floor((decimal)numberOfItems / 3);
-      decimal remainingItems = numberOfItems % 3;
-      cost = (setsOfThree * 10) + (remainingItems * 5);
+      decimal setsOfThree = Math.Floor((decimal)numberOfItems / NumberOfItemsForDeal);
+      decimal remainingItems = numberOfItems % NumberOfItemsForDeal;
+      cost = (setsOfThree * DealPrice) + (remainingItems * StandardPrice);
       return cost;
     }
   }
