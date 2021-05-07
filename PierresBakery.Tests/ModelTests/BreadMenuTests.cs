f5fe1359c7b647ad8dd7f1d4ca2a23daa8a34413
Vanessa Su <vnessa.su@gmail.com>
2021-05-07 12:02:32 -0400

    :white_check_mark: Add test for input 1 to return cost 5

--- PierresBakery.Tests/ModelTests/BreadMenuTests.cs
+++ PierresBakery.Tests/ModelTests/BreadMenuTests.cs
@@ -1,1 +1,20 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BreadMenu;
+
+namespace BreadMenu.Tests
+{
+  [TestClass]
+  public class BreadMenuTests
+  {
+    private Bread _breadObject = new Bread();
+
+    [TestMethod]
+    public void GetCost_One_Five()
+    {
+      int inputNumberOfItems = 1;
+      decimal expectedCost = 5;
+      decimal returnedCost = _breadObject.GetCost(inputNumberOfItems);
+      Assert.AreEqual(expectedCost, returnedCost);
+    }
+  }
+}
--- PierresBakery/Models/BreadMenu.cs
+++ PierresBakery/Models/BreadMenu.cs
@@ -2,6 +2,10 @@ namespace BreadMenu
 {
   public class Bread
   {
-    
+    public decimal GetCost(int numberOfItems)
+    {
+      decimal cost = 0;
+      return cost;
+    }
   }
 }
