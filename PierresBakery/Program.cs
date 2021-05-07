2297308859de6f8d0de3d7bbfaac7a7a183799f5
Vanessa Su <vnessa.su@gmail.com>
2021-05-07 15:37:06 -0400

    :hammer: Add welcome message and menu display

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using BreadMenu;
+using PastryMenu;
 
 namespace PurchaseOrder
 {
@@ -6,7 +8,23 @@ namespace PurchaseOrder
   {
     public static void Main()
     {
-      
+      decimal breadStandardPrice = 5;
+      decimal breadDealPrice = 10;
+      decimal breadNumberOfItemsForDeal = 3;
+      Bread breadItem = new Bread(breadStandardPrice, breadDealPrice, breadNumberOfItemsForDeal);
+
+      decimal pastryStandardPrice = 5;
+      decimal pastryDealPrice = 10;
+      decimal pastryNumberOfItemsForDeal = 3;
+      Pastry pastryItem = new Pastry(pastryStandardPrice, pastryDealPrice, pastryNumberOfItemsForDeal);
+
+      Console.WriteLine("|-----------------------------|");
+      Console.WriteLine("| Welcome to Pierre's Bakery! |");
+      Console.WriteLine("|-----------------------------|\n");
+      Console.WriteLine(" Menu");
+      Console.WriteLine("------");
+      Console.WriteLine("Bread \t\t${0} or Buy 2 get 1 free!", breadStandardPrice);
+      Console.WriteLine("Pastry \t\t${0} or 3 for ${1}", pastryStandardPrice, pastryDealPrice);
     }
   }
 }
