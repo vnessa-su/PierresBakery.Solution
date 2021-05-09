2a4a3247d96fa42a3b5b15685ab551ffd94de43d
Vanessa Su <vnessa.su@gmail.com>
2021-05-09 13:31:23 -0400

    :white_check_mark: Add test for add 1 bread item to order

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -5,6 +5,18 @@ namespace UserOrder.Tests
   [TestClass]
   public class OrderTests
   {
-    
+    Order _orderObject = new Order();
+
+    [TestMethod]
+    public void AddItemToList_OneBread_ItemsListWithBreadQuantityOneLinePriceFive()
+    {
+      string itemType = "bread";
+      int quantity = 1;
+      decimal expectedLinePrice = 5;
+      object[] expectedItemValues = new object[] {quantity, expectedLinePrice};
+      bool isItemAdded = _orderObject.AddItemToList(itemType, quantity);
+      Assert.IsTrue(isItemAdded);
+      Assert.AreEqual(expectedItemValues, _orderObject.ItemsList[itemType]);
+    }
   }
 }
