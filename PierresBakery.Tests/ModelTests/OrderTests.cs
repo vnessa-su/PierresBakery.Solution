bd36af17c07752811b063dad00c2bab1027c20fd
Vanessa Su <vnessa.su@gmail.com>
2021-05-09 14:06:33 -0400

    :white_check_mark: test for add 0 bread items to order

--- PierresBakery.Tests/ModelTests/OrderTests.cs
+++ PierresBakery.Tests/ModelTests/OrderTests.cs
@@ -8,6 +8,16 @@ namespace UserOrder.Tests
     Order _orderObject = new Order();
 
     [TestMethod]
+    public void AddItemToList_ZeroBread_ItemNotAddedToItemsList()
+    {
+      string itemType = "bread";
+      int quantity = 0;
+      bool isItemAdded = _orderObject.AddItemToList(itemType, quantity);
+      Assert.IsFalse(isItemAdded);
+      Assert.IsFalse(_orderObject.ItemsList.ContainsKey(itemType));
+    }
+
+    [TestMethod]
     public void AddItemToList_OneBread_ItemsListWithBreadQuantityOneLinePriceFive()
     {
       string itemType = "bread";
